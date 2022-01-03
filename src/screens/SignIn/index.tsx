1aad2c3de54e257ede3bcc6aa212f5f5c5f657cc
Hudson <hudsoneeto@outlook.com>
2022-01-03 11:22:39 -0300

    feat: interface de login completa

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,22 +1,47 @@
+import React from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+
+import brandImg from "@assets/brand.png";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import React from "react";
 
-import { Container } from "./styles";
+import {
+  Container,
+  Content,
+  Title,
+  Brand,
+  ForgostPasswordLabel,
+  ForgotPasswordButton,
+} from "./styles";
 
 export const SignIn = (): JSX.Element => {
   return (
     <Container>
-      <Input
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+      >
+        <Content>
+          <Brand source={brandImg} />
 
-      <Input placeholder="Senha" type="secondary" secureTextEntry />
+          <Title>Login</Title>
 
-      <Button title="Entrar" type="secondary" />
+          <Input
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+
+          <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+          <ForgotPasswordButton>
+            <ForgostPasswordLabel>Esqueci minha senha</ForgostPasswordLabel>
+          </ForgotPasswordButton>
+
+          <Button title="Entrar" type="secondary" />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   );
 };
